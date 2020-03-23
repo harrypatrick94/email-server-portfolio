d6be1b99f32f799143e23252f92118711c18b7ac
Harry Patrick <harry.g.patirck94@gmail.com>
2020-03-23 12:46:40 +1100

    fix email

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('Server is ready to take messagesss');
+    console.log('Server is ready to take messages');
   }
 });
 
