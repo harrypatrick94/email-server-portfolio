9086ec662db8b97237597aa32a32499cf90bc90c
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-04 10:24:45 +1100

    added send grid

--- server.js
+++ server.js
@@ -1,59 +1,92 @@
-let express = require('express');
-let router = express.Router();
-let nodemailer = require('nodemailer');
-let cors = require('cors');
-let bodyParser = require('body-parser');
-const creds = require('./config/config.js');
-const PORT = process.env.PORT
+const express = require('express');
+// const router = express.Router();
+// const nodemailer = require('nodemailer');
+const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
+const app = express()
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+app.use(cors())
+const PORT = 4000
 
-let transport = {
-    host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
-    port: 587,
-    auth: {
-    user: creds.USER,
-    pass: creds.PASS
-  }
-}
-
-let transporter = nodemailer.createTransport(transport)
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
-
-router.post('/send', (req, res, next) => {
-  let name = req.body.name
-  let email = req.body.email
-  let message = req.body.message
-  let content = `name: ${name} \n email: ${email} \n message: ${message} `
-
-  let mail = {
-    from: name,
-    to: 'harry.g.patrick94@gmail.com',  // Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
-    text: content
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
-    }
-  })
+// welcome
+app.get('/', (req, res) => {
+  res.send('Send grid email server')
 })
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.json())
-app.use('/', router)
-app.listen(PORT, () => { console.log('Server started on port')})
+app.get('/send-email', (req, res) => {
+  // variables from query string
+  const {emailTo, emailFrom, name, message} = req.query
+  console.log(emailTo);
+
+  const msg = {
+    to: emailTo,
+    from: emailFrom,
+    subject: name,
+    text: message
+    // html: 'Whats up g',
+  };
+  sgMail
+    .send(msg)
+    .then(msg => console.log(`sent to ${emailTo}`))
+    .catch(err => console.warn(err))
+})
+
+
+// app.use(bodyParser.json())
+// app.use(express.json())
+// app.use('/', router)
+app.listen(PORT, () => { console.log(`Server started on ${PORT}`)})
+// let bodyParser = require('body-parser');
+// const creds = require('./config/config.js');
+// const PORT = process.env.PORT
+// let transport = {
+//     host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+//     port: 587,
+//     auth: {
+//     user: creds.USER,
+//     pass: creds.PASS
+//   }
+// }
+//
+// let transporter = nodemailer.createTransport(transport)
+//
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Server is ready to take messages');
+//   }
+// });
+//
+// router.post('/send', (req, res, next) => {
+//   let name = req.body.name
+//   let email = req.body.email
+//   let message = req.body.message
+//   let content = `name: ${name} \n email: ${email} \n message: ${message} `
+//
+//   let mail = {
+//     from: name,
+//     to: 'harry.g.patrick94@gmail.com',  // Change to email address that you want to receive messages on
+//     subject: 'New Message from Contact Form',
+//     text: content
+//   }
+//
+//   transporter.sendMail(mail, (err, data) => {
+//     if (err) {
+//       res.json({
+//         status: 'fail'
+//       })
+//     } else {
+//       res.json({
+//        status: 'success'
+//       })
+//     }
+//   })
+// })
+
+
+
+
+
+//
+//
