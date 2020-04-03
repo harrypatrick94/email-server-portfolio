15a7fee216753544148e446b176c009b020228b1
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-04 10:26:40 +1100

    added send grid

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    USER: 'harry.g.patrick94@gmail.com',
-    PASS: 'Mk)7b653v34632#$'
-}
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-// const router = express.Router();
-// const nodemailer = require('nodemailer');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
 const app = express()
@@ -8,13 +6,13 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 app.use(cors())
 const PORT = 4000
 
-// welcome
+
 app.get('/', (req, res) => {
   res.send('Send grid email server')
 })
 
 app.get('/send-email', (req, res) => {
-  // variables from query string
+
   const {emailTo, emailFrom, name, message} = req.query
   console.log(emailTo);
 
@@ -23,7 +21,7 @@ app.get('/send-email', (req, res) => {
     from: emailFrom,
     subject: name,
     text: message
-    // html: 'Whats up g',
+
   };
   sgMail
     .send(msg)
@@ -31,62 +29,4 @@ app.get('/send-email', (req, res) => {
     .catch(err => console.warn(err))
 })
 
-
-// app.use(bodyParser.json())
-// app.use(express.json())
-// app.use('/', router)
 app.listen(PORT, () => { console.log(`Server started on ${PORT}`)})
-// let bodyParser = require('body-parser');
-// const creds = require('./config/config.js');
-// const PORT = process.env.PORT
-// let transport = {
-//     host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
-//     port: 587,
-//     auth: {
-//     user: creds.USER,
-//     pass: creds.PASS
-//   }
-// }
-//
-// let transporter = nodemailer.createTransport(transport)
-//
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Server is ready to take messages');
-//   }
-// });
-//
-// router.post('/send', (req, res, next) => {
-//   let name = req.body.name
-//   let email = req.body.email
-//   let message = req.body.message
-//   let content = `name: ${name} \n email: ${email} \n message: ${message} `
-//
-//   let mail = {
-//     from: name,
-//     to: 'harry.g.patrick94@gmail.com',  // Change to email address that you want to receive messages on
-//     subject: 'New Message from Contact Form',
-//     text: content
-//   }
-//
-//   transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         status: 'fail'
-//       })
-//     } else {
-//       res.json({
-//        status: 'success'
-//       })
-//     }
-//   })
-// })
-
-
-
-
-
-//
-//
