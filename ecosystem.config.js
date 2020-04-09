c137c2381393f814bcda27d79e174a4964040898
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-09 13:30:17 +1000

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-54-252-142-178.ap-southeast-2.compute.amazonaws.com',
-      key: '~/Desktop/portfolioEC.pem',
+      key: '~/.ssh/portfolioEC.pem',
       ref: 'origin/master',
       repo: 'https://github.com/harrypatrick94/email-server-portfolio.git',
-      path: '/home/ubuntu/server/email-server-portfolio',
+      path: '/home/ubuntu/email-server-portfolio',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
