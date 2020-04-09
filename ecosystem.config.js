2873d3361a3515ea6641f35ffca62cd33dce697f
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-09 14:35:45 +1000

    local deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/portfolioEC.pem',
       ref: 'origin/master',
       repo: 'https://github.com/harrypatrick94/email-server-portfolio.git',
-      path: '/home/ubuntu/server/email-server-portfolio',
+      path: '/home/ubuntu',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
